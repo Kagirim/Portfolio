139adcd06a596d8769f8c7af4afb4e139ccf85b0
Kagirim <morriskagiri@gmail.com>
2024-04-25 06:10:58 +0300

    change color theme

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import dotenv from "dotenv";
 import serveIndex from 'serve-index';
 import path from 'path';
 
-
+// configure environment variables
 dotenv.config();
 
 const app: Express = express();
 const PORT: string | number = process.env.PORT || 5000;
 
 // configure middleware
-app.use('/static', express.static(path.join(__dirname, '..', '/public', 'static')));
-app.use('/static', serveIndex(path.join(__dirname, '..', '/public', 'static')));
+app.use('/static', express.static(path.join(__dirname, '.', '/public', 'static')));
+console.log(path.join(__dirname, '.', '/public', 'static'));
 
 app.get('/', (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '.', 'public', 'index.html'));
     
 });
 
